feb37c3a63175e96c7b7af1cc6285edc923597c3
Daniel Morrison <daniel@collectiveidea.com>
2023-10-15 08:51:53 -0400

    Test adding middleware

--- spec/rails_app/config/initializers/twirp_rails.rb
+++ spec/rails_app/config/initializers/twirp_rails.rb
@@ -2,3 +2,7 @@ Rails.application.config.twirp.service_hooks[:before] = lambda do |rack_env, env
   # Make IP accessible to the handlers
   env[:ip] = rack_env["REMOTE_ADDR"]
 end
+
+Rails.application.config.twirp.middleware = [
+  Rack::Deflater
+]
--- spec/requests/haberdasher_spec.rb
+++ spec/requests/haberdasher_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe "Haberdasher Service", type: :request do
         "Content-Type" => "application/protobuf"
       }
     expect(response).to be_successful
+    decoded = Twirp::Example::Haberdasher::Hat.decode(response.body)
+    expect(decoded).to be_a(Twirp::Example::Haberdasher::Hat)
   end
 
   it "makes a hat" do
@@ -85,6 +87,34 @@ RSpec.describe "Haberdasher Service", type: :request do
     it "can inject data via a before hook" do
       expect(IPTracker).to receive(:track).with("127.0.0.1")
       make_hat_success_request
+    end
+  end
+
+  describe "middleware" do
+    it "runs injected middleware when appropriate" do
+      size = Twirp::Example::Haberdasher::Size.new(inches: 24)
+
+      post "/twirp/twirp.example.haberdasher.Haberdasher/MakeHat",
+        params: size.to_proto, headers: {
+          :accept => "application/protobuf",
+          "Content-Type" => "application/protobuf",
+          "Accept-Encoding" => "gzip" # ask for GZIP encoding
+        }
+      expect(response.headers["Vary"]).to eq("Accept-Encoding")
+      expect(response.headers["Content-Encoding"]).to eq("gzip")
+    end
+
+    it "ignores injected middleware when inappropriate" do
+      size = Twirp::Example::Haberdasher::Size.new(inches: 24)
+
+      post "/twirp/twirp.example.haberdasher.Haberdasher/MakeHat",
+        params: size.to_proto, headers: {
+          :accept => "application/protobuf",
+          "Content-Type" => "application/protobuf"
+          # no encoding specified
+        }
+      expect(response.headers["Vary"]).to eq("Accept-Encoding")
+      expect(response.headers["Content-Encoding"]).to be_nil
     end
   end
 end
