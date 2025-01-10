03ad117ec4411f9977b86ed8d9dca944a5c9a7e3
Daniel Morrison <daniel@collectiveidea.com>
2025-01-10 14:55:51 -0500

    Leverage Zeitwerk's for_gem_extension for nicer loading

--- lib/twirp/rails.rb
+++ lib/twirp/rails.rb
@@ -1,31 +1,24 @@
 # frozen_string_literal: true
 
-require_relative "rails/version"
-
-module Twirp
-  module Rails
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
-
 require "twirp"
 require "active_support/notifications"
-require_relative "rails/callbacks"
-require_relative "rails/configuration"
-require_relative "rails/dispatcher"
-require_relative "rails/engine"
-require_relative "rails/rescuable"
-require_relative "rails/handler"
 
-module Twirp
-  class Service
-    # Override initialize to make handler argument optional.
-    # When left nil, we will use our dispatcher.
-    alias_method :original_initialize, :initialize
-    def initialize(handler = nil)
-      handler ||= Twirp::Rails::Dispatcher.new(self.class)
-      original_initialize(handler)
-    end
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem_extension(Twirp)
+loader.ignore("#{__dir__}/on/rails.rb")
+loader.setup
+
+module Twirp::Rails
+end
+
+loader.eager_load
+
+Twirp::Service.class_eval do
+  # Override initialize to make handler argument optional.
+  # When left nil, we will use our dispatcher.
+  alias_method :original_initialize, :initialize
+  def initialize(handler = nil)
+    handler ||= Twirp::Rails::Dispatcher.new(self.class)
+    original_initialize(handler)
   end
 end
